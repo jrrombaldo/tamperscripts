97fd192658fd11cae8165bb2439ed7aef66dff36
Carlos Rombaldo Jr <jrrombaldo@users.noreply.github.com>
2022-04-28 17:36:07 +0100

    Create quip-update-font.js

---
+++ quip-update-font.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/jrrombaldo/tamperscripts/new/main
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    alert('hey there!');
+
+    // Your code here...
+})();
