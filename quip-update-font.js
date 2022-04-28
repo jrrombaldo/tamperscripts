7934af4be49df586deb2ae7d1402bf74d5e5dd7f
Carlos Rombaldo Jr <jrrombaldo@users.noreply.github.com>
2022-04-28 18:08:28 +0100

    Update quip-update-font.js

--- quip-update-font.js
+++ quip-update-font.js
@@ -6,6 +6,8 @@
 // @author       You
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @require      https://code.jquery.com/jquery-3.6.0.slim.min.js
+// @updateURL    https://github.com/jrrombaldo/tamperscripts/edit/main/quip-update-font.js
+// @downloadURL  https://github.com/jrrombaldo/tamperscripts/edit/main/quip-update-font.js
 // @grant        none
 // ==/UserScript==
 
