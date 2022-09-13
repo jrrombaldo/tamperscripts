820ae39e53780c7c19cd6a377a07f0cf14522701
Carlos Rombaldo Jr <jrrombaldo@users.noreply.github.com>
2022-09-13 16:55:03 +0100

    Update quip-update-font.js

--- quip-update-font.js
+++ quip-update-font.js
@@ -2,7 +2,7 @@
 // @name         Quip Font Update Script
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  update fonts on quip
 // @author       You
 // @match        https://quip-amazon.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
@@ -12,16 +12,14 @@
 // @grant        none
 // ==/UserScript==
 
-
 (function() {
     'use strict';
-    
        $(document).ready(function(){
             Array.from(document.getElementsByClassName('content')).forEach(element => {
-                element.style.setProperty('--theme-font-family', 'Avenir Next');
-                element.style.setProperty('--theme-font-size', '16px');
+                element.style.setProperty('--theme-font-family', 'iA Writer Quattro S');
+                element.style.setProperty('--theme-font-size', '14px');
+                console.log(element.style.cssText);
             });
         })
-    
-    
+
 })();
