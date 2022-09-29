b10186e4a40fbfebfbeb687147722dd1585d86e9
Carlos Rombaldo Jr <jrrombaldo@users.noreply.github.com>
2022-09-29 10:09:53 +0100

    Update quip-update-font.js

--- quip-update-font.js
+++ quip-update-font.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Quip Font Update Script
+// @name         Quip (Re)Styler
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  update fonts on quip
-// @author       You
+// @description  customize QUIP's style
+// @author       Carlos R
 // @match        https://quip-amazon.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @require      https://code.jquery.com/jquery-3.6.0.slim.min.js
@@ -14,29 +14,47 @@
 
 (function() {
     'use strict';
-
-    const FONT_FAMILY = 'iA Writer Quattro S';
-
-    const element = document.querySelector('.document-content'); // '.document-content' also works, need to understand the difference...
-    element.style.setProperty('--theme-font-family', FONT_FAMILY);
-    element.style.setProperty('--quipUiFontFamily', FONT_FAMILY);
-    element.style.setProperty('--spreadsheet-theme-font-family', FONT_FAMILY);
-    element.style.setProperty('--theme-font-size', '14px');
-
+    console.log('STARTING FONT CHANGE...');
+    
+    
+    const FONT_FAMILY = 'iA Writer Duo S';
+    const FONT_SIZE = '16px;';
+    const BACKGROUND_COLOR = "227,227,226"
+    
+  
     /*
+      some fonts on brew ...
+      brew tap homebrew/cask-fonts
       brew install font-ia-writer-quattro
       brew install font-ia-writer-duo
       brew install font-ia-writer-mono
     */
 
 
-       $(document).ready(function(){
-           /*
-            Array.from(document.getElementsByClassName('content')).forEach(element => {
-                element.style.setProperty('--theme-font-family', 'iA Writer Quattro S');
-                element.style.setProperty('--theme-font-size', '14px');
-            });
-            */
-       })
+    $(document).ready(function(){
 
+        document.body.style.setProperty('--quipBackground', BACKGROUND_COLOR);
+
+        const element = document.querySelector('.document-content'); // '.document' also works, need to understand the difference...
+        if (element == null) {
+            console.error ('ELEMENT IS NULL, CSS CLASS NOT FOUND...')
+            
+        } else {
+            element.style.setProperty('--theme-font-family', FONT_FAMILY);
+            element.style.setProperty('--quipUiFontFamily', FONT_FAMILY);
+            element.style.setProperty('--spreadsheet-theme-font-family', FONT_FAMILY);
+            element.style.setProperty('--theme-font-size', FONT_SIZE);
+        }
+
+
+        console.log('FONT CHANGED !!!');
+
+
+       /*
+        Array.from(document.getElementsByClassName('content')).forEach(element => {
+            element.style.setProperty('--theme-font-family', FONT_FAMILY);
+            element.style.setProperty('--theme-font-size', FONT_SIZE);
+        });
+        */
+     })
 })();
